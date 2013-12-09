f25a37660eaf67672de11f41700b77e26cb6f842
astronaughts <hello@astronaughts.net>
2013-12-09 19:09:28 +0900

    bump version to v0.2.2

--- example/bots/github.js
+++ example/bots/github.js
@@ -1,11 +1,18 @@
 module.exports = function (chabot) {
 
-    var endpoint = '/rooms/' + chabot.roomid + '/messages',
-        template = chabot.readTemplate('github.ejs');
+    // WebHook で受けたデータをセット
+    var payload = JSON.parse(chabot.data.payload);
+    // ChatWork API の endpoint をセット
+    var endpoint = '/rooms/' + chabot.roomid + '/messages';
+    // templats/ 内のメッセージテンプレートを読み込む
+    var template = chabot.readTemplate('github.ejs');
+    // WebHook で受けたデータでメッセージテンプレートを描画
+    var message_body = chabot.render(template, payload);
 
+    // ChatWork API でメッセージ送信
     chabot.client
         .post(endpoint, {
-            body: chabot.render(template, JSON.parse(chabot.data.payload));
+            body: message_body
         })
         .done(function (res) {
             chabot.log('done');
