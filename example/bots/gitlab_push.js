de2902912cba8cc35d3deb8b48ef2c61a9e6453d
ledsun <shigeru.nakajima@gmail.com>
2014-04-23 20:05:46 +0900

    Add a new bot for pull events of gitlab.

---
+++ example/bots/gitlab_push.js
@@ -0,0 +1,24 @@
+module.exports = function (chabot) {
+
+   // WebHook で受けたデータをセット
+    var payload = chabot.data;
+ 
+    // ChatWork API の endpoint をセット
+    var endpoint = '/rooms/' + chabot.roomid + '/messages';
+    // templats/ 内のメッセージテンプレートを読み込む
+    var template = chabot.readTemplate('gitlab_push.ejs');
+    // WebHook で受けたデータでメッセージテンプレートを描画
+    var message_body = chabot.render(template, payload);
+
+    // ChatWork API でメッセージ送信
+    chabot.client
+        .post(endpoint, {
+            body: message_body
+        })
+        .done(function (res) {
+            chabot.log('done');
+        })
+        .fail(function (err) {
+            chabot.error(err);
+        });
+};
